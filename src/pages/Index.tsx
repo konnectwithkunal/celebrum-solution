ac1d68839382e266a02e3bbff24e170014941b93
kunal <konnectwithkunal@gmail.com>
2025-11-12 12:04:34 +0530

    index

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,18 +1,12 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import HeaderHome from "@/components/HeaderHome";
-import Header from "@/components/Header";
+
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Services from "@/components/Services";
-import Clients from "@/components/Clients";
-import EventsLaunches from "@/components/EventsLaunches";
-import WhyChoose from "@/components/WhyChoose";
-import Process from "@/components/Process";
-import HowWeHelp from "@/components/HowWeHelp";
-import Testimonials from "@/components/Testimonials";
-import Work from "@/components/Work";
-import News from "@/components/News";
+
+
 import Footer from "@/components/Footer";
 import Products from "@/components/products";
 import Servicecomp from "@/components/Servicecomp"
